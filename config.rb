41d0e4da458fc8dcb107caaa01487354bf9cdcd9
Thomas Langford <thomaslangford@thomas-iMac.local>
2018-12-17 18:18:18 -0600

    Added grunt.

---
+++ config.rb
@@ -0,0 +1,10 @@
+# require "susy"
+# require "breakpoint"
+http_path ="/"
+css_dir = 'src/css'
+sass_dir = 'src/scss/masterImporter'
+javascript_dir = 'src/assets/js'
+images_dir = 'src/assets/images'
+sourcemap = true
+output_style = :expanded
+line_comments = false
---
+++ gruntfile.js
@@ -0,0 +1,33 @@
+var os = require('os'); os.tmpDir = os.tmpdir;
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-scss-lint');
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    compass: {
+      dev: {
+        options: {
+          config: 'config.rb'
+        }//options
+      }//dev
+    },//compass    
+    watch: {
+      sass: {
+        files: ['src/scss/*.scss'],
+        tasks: ['compass:dev', 'scsslint']
+      }//sass
+    },//watch
+    scsslint: {
+      allFiles: [
+        'src/scss/*.scss'
+      ],
+      options: {
+        bundleExec: false,
+        force: true
+      }
+    }//scsslint
+  });//initConfig
+  grunt.registerTask('default', ['watch', 'scsslint']);
+};//exports
