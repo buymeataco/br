9403a4fa32b6f4e954671ca49945554945bba73f
Thomas Langford <thomaslangford@thomas-iMac.local>
2018-12-17 21:11:11 -0600

    Started the list ppage but decided to setup the navbar first. Remember to rebase when returning to finish this component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
 import { CommonModule } from '@angular/common';
+import { ListComponent } from './components/list/list.component';
 
 const routes: Routes = [
-	
+	{path: 'list', component: ListComponent}
 ];
-
 
 @NgModule({
 	exports: [RouterModule],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
